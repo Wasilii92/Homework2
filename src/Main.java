7696ce9ba3dee372662f9010c474da33ed97d73e
Wasilii92 <eup104st@yandex.ru>
2024-09-18 23:44:03 +0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double dog=8;
+        var cat =3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+dog=dog+4;
+cat=cat+4;
+paper = paper+4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog =dog-3.5;
+        cat=cat-1.6;
+        paper = paper-7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend=19;
+        System.out.println(friend);
+         friend=(friend+2)/7;
+        System.out.println(friend);
+      var frog = 3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        var mas1=78.2;
+        var mas2 = 82.7;
+        var masObch=mas1+mas2;
+        var razn=mas2-mas1;
+        System.out.println(masObch);
+        System.out.println(razn);
+        var masOst=mas2%mas1;
+        System.out.println(masOst);
+        var obsch=640;
+        var norm=8;
+        var kSotr=640/8;
+        System.out.print("Всего работников в компании -");
+    System. out. print( kSotr);
+        System.out.println(" человек");
+        var newKSotr=kSotr+94;
+        var newObsch=newKSotr*8;
+        System.out.print("Если в компании работает ");
+        System.out.print(newKSotr);
+        System.out.print(" то всего ");
+        System.out.print(newObsch);
+        System.out.print(" часов может быть поделено между сотрудниками");
     }
+
 }
